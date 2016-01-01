1d15e4f942da1966c0ab7cc5def5ade69b7f17d6
sabazusi <kouhei.live@gmail.com>
2016-01-01 23:48:25 +0900

    initial commit.

---
+++ MainCharacterController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacterController : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		// simple rotation character.
+		transform.Rotate(0, 1, 0);
+	}
+}
