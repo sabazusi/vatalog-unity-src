aae484d431f7320d341ce5c0c403473a9e7cc421
sabazusi <kouhei.live@gmail.com>
2016-01-02 18:08:56 +0900

    add simple title script.

---
+++ Title.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	void Start () {
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+	//	if (Input.touchCount > 0)
+		{
+			Application.LoadLevel("main");
+		}
+	}
+}
